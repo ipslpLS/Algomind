63f9f196c0b4f99909f8b99b4c75ec618dca36e2
Lavrente Simion <simion.lavrente@gmail.com>
2024-01-19 11:17:10 +0200

    Updated search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const searchInput = document.getElementById("search-input");
     const searchResults = document.getElementById("search-results");
   
-    fetch("/Algomind/blob/main/content/data/search.json")
+    fetch("https://ipslpls.github.io/Algomind/content/data/search.json")
       .then(response => {
         if (!response.ok) {
           throw new Error(`Network response was not ok: ${response.status}`);
