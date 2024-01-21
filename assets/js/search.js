8a520dce1d950a18be62acad5363a02e887112f5
Lavrente Simion <simion.lavrente@gmail.com>
2024-01-21 10:50:09 +0200

    Changed search.json directory

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const searchInput = document.getElementById("search-input");
     const searchResults = document.getElementById("search-results");
   
-    fetch("/articles/search.json")
+    fetch("/search.json")
       .then(response => {
         if (!response.ok) {
           throw new Error(`Network response was not ok: ${response.status}`);
